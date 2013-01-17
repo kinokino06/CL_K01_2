f28b2b19d08157a70116f5a0f6db22cbdc5d2ba2
ito jun <tabero443@gmail.com>
2013-01-17 11:28:07 +0900

    Revert "応用プログラミングAの課題１"
    This reverts commit 7a1a0c8492ebf878e36b89fdf3ec86a689acb8b7.

--- CL_K01_2/CL_K01_2.c
+++ CL_K01_2/CL_K01_2.c
@@ -1,9 +1,9 @@
-﻿/*作成者:1T4(2) 伊藤純 */
-/*課題名:課題01_2 */
+/*�쐬��:1T4(2) �ɓ��� */
+/*�ۑ薼:�ۑ�01_2 */
 void main(void) {
 int width, i;
 width = 5;
 i = width * width;
-	printf("一辺の長さ%dの正方形の面積は%dです。\n", width,i);
-	printf("ごせえええ \n");
+	printf("��ӂ̒���%d�̐����`�̖ʐς�%d�ł��B\n", width,i);
+	printf("���������� \n");
 }
