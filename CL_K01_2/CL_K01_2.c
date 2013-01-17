591ce742f8ebd53c86561ffb9f1d362604458c13
ito jun <tabero443@gmail.com>
2013-01-17 11:20:15 +0900

    応用プログラミングA の課題1

---
+++ CL_K01_2/CL_K01_2.c
@@ -0,0 +1,9 @@
+/*ì¬Ò:1T4(2) ˆÉ“¡ƒ */
+/*‰Û‘è–¼:‰Û‘è01_2 */
+void main(void) {
+int width, i;
+width = 5;
+i = width * width;
+	printf("ˆê•Ó‚Ì’·‚³%d‚Ì³•ûŒ`‚Ì–ÊÏ‚Í%d‚Å‚·B\n", width,i);
+	printf("‚² \n");
+}
